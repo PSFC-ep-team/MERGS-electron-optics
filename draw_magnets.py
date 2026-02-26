deeb061fa85bb0a2eda60bba7f5814c7bb870c36
Justin Kunimune <justinkunimune@gmail.com>
2026-02-26 17:09:14 -0500

    update electron optics design
    I added some constraints and re-ran the optimization.  I don't think the new constraints really affected anything but it did find a new optimum with a thinner dipole.  I also made the picture more accurate.

--- draw_magnets.py
+++ draw_magnets.py
@@ -1,9 +1,9 @@
 from __future__ import annotations
 
 import re
-from typing import Tuple, Dict, List
+from typing import Tuple, Dict, List, Optional
 
-from numpy import sin, cos, pi, zeros_like, linspace, hypot
+from numpy import sin, cos, pi, zeros_like, linspace, hypot, inf, where
 
 FILE_TO_OPTIMIZE = "mergs_ion_optics"
 PARAMETER_STRING = """
@@ -11,24 +11,30 @@ foil_width := 0.3000000E-01;
 foil_height := 0.3000000E-01;
 aperture_width := 0.3000000E-01;
 aperture_height := 0.3000000E-01;
-p_m5_quad_field := 0.4093343E-01;
+p_m5_quad_field := 0.4781922E-01;
 p_m5_hex_field :=  0.000000;
-p_dipole_field := 0.3123733;
-p_m5_radius := 0.2748934E-01;
-p_m5_length := 0.1099574;
+p_dipole_field := 0.3201551;
+p_m5_radius := 0.2927906E-01;
+p_m5_length := 0.1171162;
 p_dipole_halfwidth := 0.1300000;
-p_dipole_length := 0.2038280;
+p_dipole_length := 0.1982885;
 p_drift_pre_aperture := 0.5000000;
-p_drift_pre_bend := 0.2034160;
-p_drift_post_bend := 0.4658314;
-p_shape_in_1 := 0.5945187;
-p_shape_in_2 :=  5.761873;
-p_shape_in_3 :=  0.000000;
-p_shape_out_1 := 0.4287605;
-p_shape_out_2 := -3.438266;
-p_shape_out_3 :=  0.000000;
-dipole_bend_angle :=  78.05899;
-dipole_bend_radius := 0.1496110;
+p_drift_pre_bend := 0.2240527;
+p_drift_post_bend := 0.5887938;
+p_shape_in_1 := 0.6509721;
+p_shape_in_2 :=  5.195103;
+p_shape_in_3 :=  4.456388;
+p_shape_out_1 := 0.4589667;
+p_shape_out_2 := -2.745608;
+p_shape_out_3 := -1.355867;
+
+dipole_bend_angle :=  77.82928;
+dipole_max_bend_radius := 0.2745744;
+dipole_central_bend_radius := 0.1459745;
+dipole_min_bend_radius := 0.7757664E-01;
+dipole_gap_height := 0.3994749E-01;
+hodoscope_right := 0.5552835;
+hodoscope_left := 0.1747930;
 """
 CENTRAL_ENERGY = 13.5
 
@@ -41,8 +47,8 @@ def draw_magnets():
 	parameters = parse_parameters(PARAMETER_STRING)
 
 	paths = []
-	x = .10
-	y = .10
+	x = .15
+	y = .15
 	θ = 0
 
 	draw_plane(
@@ -70,7 +76,9 @@ def draw_magnets():
 		paths, x, y, θ,
 		parameters["p_dipole_length"],
 		parameters["p_dipole_field"],
-		0.13,
+		parameters["dipole_min_bend_radius"],
+		parameters["dipole_max_bend_radius"],
+		parameters["dipole_gap_height"],
 		[
 			parameters["p_shape_in_1"],
 			parameters["p_shape_in_2"],
@@ -86,7 +94,11 @@ def draw_magnets():
 		paths, x, y, θ,
 		parameters["p_drift_post_bend"],
 	)
-	draw_plane(paths, x, y, θ, 0.05)
+	draw_plane(
+		paths, x, y, θ,
+		parameters["hodoscope_left"],
+		parameters["hodoscope_right"],
+	)
 
 	write_SVG("picture.svg", paths)
 
@@ -102,11 +114,13 @@ def parse_parameters(output: str) -> Dict[str, float]:
 
 
 def draw_plane(
-		graphic: List[Path], x: float, y: float, θ: float, radius: float
+		graphic: List[Path], x: float, y: float, θ: float, left: float, right: Optional[float] = None,
 ) -> None:
+	if right is None:
+		right = left
 	line = [
-		("M", [x + radius*sin(θ), y - radius*cos(θ)]),
-		("L", [x - radius*sin(θ), y + radius*cos(θ)]),
+		("M", [x + left*sin(θ), y - left*cos(θ)]),
+		("L", [x - right*sin(θ), y + right*cos(θ)]),
 	]
 	graphic.append(Path(klass="plane", commands=line, zorder=1))
 
@@ -148,85 +162,121 @@ def draw_multipole_magnet(
 
 def draw_bending_magnet(
 		graphic: List[Path], x: float, y: float, θ: float,
-		length: float, field: float, bore_radius: float,
+		length: float, field: float, min_bend_radius: float, max_bend_radius: float, gap_height: float,
 		in_shape_parameters: List[float], out_shape_parameters: List[float],
 ) -> Tuple[float, float, float]:
 	central_momentum = (0.5110 + CENTRAL_ENERGY)*1.602e-13/2.998e8  # kg*m/s
-	bend_radius = central_momentum/(1.602e-19*field)  # m
-	bend_angle = length/bend_radius  # radians
-	x_center = x - bend_radius*sin(θ)
-	y_center = y + bend_radius*cos(θ)
+	central_bend_radius = central_momentum/(1.602e-19*field)  # m
+	bend_angle = length/central_bend_radius  # radians
+	x_center = x - central_bend_radius*sin(θ)
+	y_center = y + central_bend_radius*cos(θ)
 
-	ξ = linspace(-bore_radius, bore_radius, 21)
-	ζ_back = evaluate_polynomial(ξ, [0] + in_shape_parameters)
-	x_back = x_center + (bend_radius + ξ)*sin(θ) + ζ_back*cos(θ)
-	y_back = y_center - (bend_radius + ξ)*cos(θ) + ζ_back*sin(θ)
+	min_extended_radius = min_bend_radius - 1.5*gap_height
+	max_extended_radius = max_bend_radius + 1.5*gap_height
+
+	ξ = linspace(min_extended_radius - central_bend_radius, max_extended_radius - central_bend_radius, 21)
+	ζ_back = evaluate_polynomial(
+		ξ, [0] + in_shape_parameters,
+		lower_breakpoint=min_bend_radius - central_bend_radius, upper_breakpoint=max_bend_radius - central_bend_radius)
+	x_back = x_center + (central_bend_radius + ξ)*sin(θ) + ζ_back*cos(θ)
+	y_back = y_center - (central_bend_radius + ξ)*cos(θ) + ζ_back*sin(θ)
 	R_back = hypot(x_back - x_center, y_back - y_center)
-	within_radius = R_back <= bend_radius + bore_radius
+	within_radius = R_back <= max_extended_radius
 	x_back = x_back[within_radius]
 	y_back = y_back[within_radius]
-	ζ_front = -evaluate_polynomial(ξ, [0] + out_shape_parameters)
-	x_front = x_center + (bend_radius + ξ)*sin(θ + bend_angle) + ζ_front*cos(θ + bend_angle)
-	y_front = y_center - (bend_radius + ξ)*cos(θ + bend_angle) + ζ_front*sin(θ + bend_angle)
+	ζ_front = -evaluate_polynomial(
+		ξ, [0] + out_shape_parameters,
+		lower_breakpoint=min_bend_radius - central_bend_radius, upper_breakpoint=max_bend_radius - central_bend_radius)
+	x_front = x_center + (central_bend_radius + ξ)*sin(θ + bend_angle) + ζ_front*cos(θ + bend_angle)
+	y_front = y_center - (central_bend_radius + ξ)*cos(θ + bend_angle) + ζ_front*sin(θ + bend_angle)
 	R_front = hypot(x_front - x_center, y_front - y_center)
-	within_radius = R_front <= bend_radius + bore_radius
+	within_radius = R_front <= max_extended_radius
 	x_front = x_front[within_radius]
 	y_front = y_front[within_radius]
 
 	block = [
 		("M", [x_back[-1], y_back[-1]]),
 		("A", [
-			bend_radius + bore_radius, bend_radius + bore_radius,
+			max_extended_radius, max_extended_radius,
 			0, (1 if bend_angle > pi else 0), 1,
 			x_front[-1], y_front[-1],
 		]),
 		*[("L", [x, y]) for x, y in zip(x_front[-2::-1], y_front[-2::-1])],
 		("L", [
-			x_center + (bend_radius - bore_radius)*sin(θ + bend_angle),
-			y_center - (bend_radius - bore_radius)*cos(θ + bend_angle),
+			x_center + min_extended_radius*sin(θ + bend_angle),
+			y_center - min_extended_radius*cos(θ + bend_angle),
 		]),
 		("A", [
-			bend_radius - bore_radius, bend_radius - bore_radius,
+			min_extended_radius, min_extended_radius,
 			0, (1 if bend_angle > pi else 0), 0,
-			x_center + (bend_radius - bore_radius)*sin(θ),
-			y_center - (bend_radius - bore_radius)*cos(θ),
+			x_center + min_extended_radius*sin(θ),
+			y_center - min_extended_radius*cos(θ),
 		]),
 		*[("L", [x, y]) for x, y in zip(x_back, y_back)],
 		("Z", []),
 	]
 	graphic.append(Path(klass="magnet", commands=block, zorder=1))
 
-	arc = [
-		("M", [x, y]),
-		("A", [
-			bend_radius, bend_radius,
-			0, (1 if bend_angle > pi else 0), 1,
-			x_center + bend_radius*sin(θ + bend_angle),
-			y_center - bend_radius*cos(θ + bend_angle),
-		]),
-	]
-	graphic.append(Path(klass="central-ray", commands=arc, zorder=2))
+	for radius, klass in [(max_bend_radius, "guide"), (min_bend_radius, "guide"), (central_bend_radius, "central-ray")]:
+		arc = [
+			("M", [x_center + radius*sin(θ), y_center - radius*cos(θ)]),
+			("A", [
+				radius, radius,
+				0, (1 if bend_angle > pi else 0), 1,
+				x_center + radius*sin(θ + bend_angle),
+				y_center - radius*cos(θ + bend_angle),
+			]),
+		]
+		graphic.append(Path(klass=klass, commands=arc, zorder=2))
 
 	x, y = arc[-1][1][-2:]
 	θ = θ + bend_angle
 	return x, y, θ
 
 
-def evaluate_polynomial(x, coefficients):
-	y = zeros_like(x)
+def evaluate_polynomial(x, coefficients, lower_breakpoint=-inf, upper_breakpoint=inf):
+	"""
+	evaluate the polynomial defined by some coefficients.  if you want, you can also specify upper and lower
+	"breakpoints"; outside of these breakpoints, all derivatives greater than 1 will be set to zero.
+	"""
+	y_middle = zeros_like(x)
 	for i, coefficient in enumerate(coefficients):
-		y += coefficient*x**i
-	return y
+		y_middle += coefficient*x**i
+
+	y0_lower = 0
+	m_lower = 0
+	for i, coefficient in enumerate(coefficients):
+		y0_lower += coefficient*lower_breakpoint**i
+		m_lower += coefficient*i*lower_breakpoint**(i - 1)
+	y_lower = m_lower*(x - lower_breakpoint) + y0_lower
+
+	y0_upper = 0
+	m_upper = 0
+	for i, coefficient in enumerate(coefficients):
+		y0_upper += coefficient*upper_breakpoint**i
+		m_upper += coefficient*i*upper_breakpoint**(i - 1)
+	y_upper = m_upper*(x - upper_breakpoint) + y0_upper
+
+	return where(
+		x < lower_breakpoint,
+		y_lower,
+		where(
+			x < upper_breakpoint,
+			y_middle,
+			y_upper,
+		)
+	)
 
 
 def write_SVG(filename: str, paths: List[Path]) -> None:
 	svg_string = (
 		'<?xml version="1.0" encoding="UTF-8"?>\n'
-		'<svg xmlns="http://www.w3.org/2000/svg" version="1.1" viewBox=".00 .00 2.00 1.00" width="2m" height="1m">\n'
+		'<svg xmlns="http://www.w3.org/2000/svg" version="1.1" viewBox=".00 .00 1.00 1.00" width="1m" height="1m">\n'
 		'  <style>\n'
 		'    .magnet { fill: #8b959e; stroke: none; }\n'
 		'    .plane { fill: none; stroke: #8b959e; stroke-width: .01; stroke-linecap: butt; }\n'
 		'    .central-ray { fill: none; stroke: #750014; stroke-width: .01; stroke-linecap: round; }\n'
+		'    .guide { fill: none; stroke: #ffffff; stroke-width: .005; stroke-linecap: butt; stroke-dasharray: .01 }\n'
 		'  </style>\n'
 	)
 
