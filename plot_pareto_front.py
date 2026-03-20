be771554960b4f1db8aa58a1258635c2de996961
Justin Kunimune <justinkunimune@gmail.com>
2026-03-20 13:13:45 -0400

    plot aperture efficiency curve
    it turns out the requirements we set are much harder to hit than I expected.  I hadn't actually calculated how the efficiency I was getting compared to the requirement, but now I have and it turns out I need a bigger aperture than 3 cm 50 cm away.

--- plot_pareto_front.py
+++ plot_pareto_front.py
@@ -10,23 +10,30 @@ from typing import Sequence
 import matplotlib.pyplot as plt
 import numpy as np
 from numpy import geomspace, stack, zeros_like
+from numpy.ma.core import empty_like
 from scipy import optimize
 
-from hyperparameters import calculate_resolution, optimize_foil_thickness
+from hyperparameters import calculate_resolution, optimize_foil_thickness, calculate_foil_resolution
 
 
-def plot_pareto_front(*filenames: str):
+def plot_pareto_fronts(*designs: str | tuple[float, float, float]):
 	fronts = []
 
-	for filename in filenames:
-		if os.path.isfile(f"generated/{filename}_pareto_front.txt"):
-			resolutions, efficiencies = np.loadtxt(f"generated/{filename}_pareto_front.txt", unpack=True)
+	for design in designs:
+		if type(design) is str:
+			name = str(design)
+			if os.path.isfile(f"generated/{name}_pareto_front.txt"):
+				resolutions, efficiencies = np.loadtxt(f"generated/{name}_pareto_front.txt", unpack=True)
+			else:
+				resolutions, efficiencies = find_pareto_front_of_magnet_design(name)
+				np.savetxt(
+					f"generated/{name}_pareto_front.txt",
+					stack([resolutions, efficiencies], axis=1))
 		else:
-			resolutions, efficiencies = find_pareto_front(filename)
-			np.savetxt(
-				f"generated/{filename}_pareto_front.txt",
-				stack([resolutions, efficiencies], axis=1))
-		fronts.append((resolutions, efficiencies))
+			foil_diameter, aperture_distance, aperture_diameter = design
+			resolutions, efficiencies = find_pareto_front_of_aperture_design(foil_diameter, aperture_distance, aperture_diameter)
+			name = f"{foil_diameter*100}-{aperture_distance*100}-{aperture_diameter*100}"
+		fronts.append((resolutions, efficiencies, name))
 
 	plt.rcParams["font.size"] = 12
 	plt.rcParams['xtick.labelsize'] = 12
@@ -34,8 +41,10 @@ def plot_pareto_front(*filenames: str):
 
 	plt.figure(figsize=(4.5, 4.0))
 	plt.grid()
-	for resolutions, efficiencies in fronts:
-		plt.plot(resolutions, efficiencies)
+	for resolutions, efficiencies, name in fronts:
+		plt.plot(resolutions, efficiencies, label=name)
+	if len(fronts) > 1:
+		plt.legend()
 	plt.xlim(0, 500)
 	plt.yscale("log")
 	plt.ylim(0.1, 10)
@@ -46,15 +55,25 @@ def plot_pareto_front(*filenames: str):
 	plt.show()
 
 
-def find_pareto_front(filename: str) -> tuple[Sequence[float], Sequence[float]]:
+def find_pareto_front_of_aperture_design(foil_diameter: float, aperture_distance: float, aperture_diameter: float) -> tuple[Sequence[float], Sequence[float]]:
+	efficiencies = geomspace(0.1, 10, 11)  # counts/MJ
+	resolutions = empty_like(efficiencies)
+	with ProcessPoolExecutor(max_workers=8) as executor:
+		for i, efficiency in enumerate(efficiencies):
+			resolutions[i] = calculate_foil_resolution(
+				optimize_foil_thickness(foil_diameter, aperture_distance, aperture_diameter, efficiency, executor))
+	return resolutions, efficiencies
+
+
+def find_pareto_front_of_magnet_design(filename: str) -> tuple[Sequence[float], Sequence[float]]:
 	with open(f"{filename}.fox") as file:
 		script_content = file.read()
 	max_foil_diameter = float(re.search(r"foil_width := ([0-9.]+)", script_content).group(1))
 	max_aperture_diameter = float(re.search(r"aperture_width := ([0-9.]+)", script_content).group(1))
 	aperture_distance = float(re.search(r"drift_pre_aperture := ([0-9.]+)", script_content).group(1))
 
-	# n.b. 0.1 counts/MJ means that we can make a ±10% measurement every 10 seconds at 10 MW operation,
-	# and 10 counts/MJ means that we can make ten ±10% measurements every second at 100 MW operation
+	# n.b. 0.1 counts/MJ means that we can make a ±10% measurement every 10 seconds at 100 MW operation,
+	# and 10 counts/MJ means that we can make a ±10% measurement every second at 10 MW operation
 	efficiencies = geomspace(0.1, 10, 11)  # counts/MJ
 	results: list[Future] = []
 
@@ -108,4 +127,4 @@ def find_suitable_hyperparameters(
 
 
 if __name__ == "__main__":
-	plot_pareto_front("mergs_electron_optics")
+	plot_pareto_fronts((.03, .50, .03), (.03, .25, .04))
